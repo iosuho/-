87b6d7b454cb18c297f29dcaaad08361ccbe0544
Vladimir <proydakov99@bk.ru>
2026-05-13 17:45:05 +0300

    Initial Commit

---
+++ Куда гоним, шо/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Куда гоним, шо
+//
+//  Created by Владимир Пройдаков on 13.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
